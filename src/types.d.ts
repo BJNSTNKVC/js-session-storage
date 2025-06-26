62fe26c1b051a5fceab474355bcb7f00f4625e91
Bojan Stankovic <dev.bojan.stankovic@gmail.com>
2025-06-26 23:10:50 +0200

    #14 Updated package dependencies and added package module and class declarations/exports.

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,21 @@
 type SessionStorageItem = {
     data: any,
 };
+
+declare module '@bjnstnkvc/session-storage' {
+    export class SessionStorage {
+        static set(key: string, value: any): void;
+        static get(key: string, fallback?: string | Function | null): any;
+        static remember(key: string, callback: Function): any;
+        static all(): object;
+        static remove(key: string): void;
+        static clear(): void;
+        static has(key: string): boolean;
+        static hasAny(keys: string | string[]): boolean;
+        static isEmpty(): boolean;
+        static isNotEmpty(): boolean;
+        static keys(): string[];
+        static count(): number;
+        static dump(key: string): void;
+    }
+}
